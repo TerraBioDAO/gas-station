ac42a8127e5bf97e3ba9a01f0065c6fe88fd6272
Mickael <koutakou@protonmail.com>
2022-07-31 23:00:01 +0200

    pnpm instead yarn + terrabiodao rpc

--- src/web3.js
+++ src/web3.js
@@ -1,8 +1,8 @@
 import Web3 from 'web3';
 
 const web3 = {
-    astar: new Web3('wss://rpc.astar.network'),
-    shibuya: new Web3('wss://rpc.shibuya.astar.network'),
+    astar: new Web3('wss://wss-astar-node.terrabiodao.org'),
+    shibuya: new Web3('wss://wss-shibuya-node.terrabiodao.org'),
     shiden: new Web3('wss://rpc.shiden.astar.network'),
 };
 
